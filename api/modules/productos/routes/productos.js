d682853c7b4daa915c024cb42423ec6585abfe8d
D-Perez85 <damianperez22@hotmail.com>
2021-09-20 18:01:31 -0300

    Add metodo y verbo DELETE en Modulo Productos

--- api/modules/productos/routes/productos.js
+++ api/modules/productos/routes/productos.js
@@ -11,11 +11,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const express = require("express");
 const productosSchema = require("../schemas/productos");
 const router = express.Router();
+router.post("/producto", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const newProduct = req.body;
+        const productos = new productosSchema.productos(newProduct);
+        const productoNuevo = yield productos.save();
+        console.log("Producto agregado", productoNuevo);
+        return res.status(200).send({ status: "success", data: productoNuevo });
+    }
+    catch (err) {
+        console.log("Error: ", err);
+        return res.status(404).send({ status: "error", data: err });
+    }
+}));
 router.get("/productos", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         let productos = yield productosSchema.productos.find();
         console.log("Productos registrados!!!", productos);
         return res.status(200).send({ status: "success", data: productos });
+    }
+    catch (err) {
+        console.log("Error: ", err);
+        return res.status(404).send({ status: "error", data: err });
+    }
+}));
+router.put("/producto/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const productUpdate = req.body;
+        const idProduct = req.params.id;
+        const productUpdated = yield productosSchema.productos.findByIdAndUpdate(idProduct, productUpdate, { new: true });
+        console.log("Producto modificado", productUpdated);
+        return res.status(200).send({ status: "success", data: productUpdated });
+    }
+    catch (err) {
+        console.log("Error: ", err);
+        return res.status(404).send({ status: "error", data: err });
+    }
+}));
+router.delete("/producto/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const productDelete = req.body;
+        const idProduct = req.params.id;
+        const productDeleted = yield productosSchema.productos.findByIdAndDelete(idProduct, productDelete);
+        console.log("Producto Borrado", productDeleted);
+        return res.status(200).send({ status: "success", data: productDeleted });
     }
     catch (err) {
         console.log("Error: ", err);
